5268cdeca5bde9d8952be25c70fd71062675e8f5
Daniil Parakhin <daniilparahin35@gmail.com>
2025-12-24 12:27:46 +0300

    Вывели результат вычисления на экран.

--- VolkovaPr6/Program.cs
+++ VolkovaPr6/Program.cs
@@ -14,6 +14,7 @@ namespace VolkovaPr6
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
             double F = (Math.Sin(x) + Math.Cos(x) / 2);
+            Console.WriteLine($"Для значения х = {x}, F = {F}.");
         }
     }
 }
